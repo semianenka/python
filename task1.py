9ba5e7315fe6b72014c0d022c2455245839eb9f4
Stanislau Semianenka <stanislau.semianenka@itechart-group.com>
2021-06-22 10:33:09 +0300

    perfected(I hope)

--- task1.py
+++ task1.py
@@ -15,12 +15,10 @@ CONSONANTS = 'bcdfgjklmnpqstvxzhrw'
 
 
 def sort_by_length(list_):
-    new_list = list_[:]
-    new_list.sort(key=len)
-    return new_list
+    return sorted(list_, key=len)
 
 
-def sort_by_letters(list_, by_vowels=True):
+def sort_by_letters(list_, letters_list):
     def letters_count(string, source):
         count = 0
         string = string.lower()
@@ -29,35 +27,26 @@ def sort_by_letters(list_, by_vowels=True):
                 count += string.count(vowel)
         return count
 
-    new_list = list_[:]
-    if by_vowels:
-        new_list.sort(key=lambda string: letters_count(string, VOWELS))
-    else:
-        new_list.sort(key=lambda string: letters_count(string, CONSONANTS))
-    return new_list
+    return sorted(list_, key=lambda string: letters_count(string, letters_list))
 
 
 def swap_first_last_letter(list_):
     new_list = list_[:]
-    for i in range(len(new_list)):
-        if i % 2:
-            strings = list(new_list[i])
-            strings[0], strings[-1] = strings[-1], strings[0]
-            new_list[i] = ''.join(map(str, strings))
+    for i in range(1, len(new_list), 2):
+        strings = list(new_list[i])
+        strings[0], strings[-1] = strings[-1], strings[0]
+        new_list[i] = ''.join(strings)
     return new_list
 
 
 def revert_list_of_strings(list_):
-    new_list = list_[:]
-    for i in range(len(new_list)):
-        new_list[i] = new_list[i][::-1]
-    return new_list
+    return [i[::-1] for i in list_]
 
 
 print(f'Find the second by length string in a list or array:\n{sort_by_length(list_of_strings)[1]}')
 print(f'Sort list by string length:\n{sort_by_length(list_of_strings)}')
-print(f'Sort list by count of vowels in string:\n{sort_by_letters(list_of_strings)}')
-print(f'Sort list by count of consonants in string:\n{sort_by_letters(list_of_strings, False)}')
+print(f'Sort list by count of vowels in string:\n{sort_by_letters(list_of_strings, VOWELS)}')
+print(f'Sort list by count of consonants in string:\n{sort_by_letters(list_of_strings, CONSONANTS)}')
 print(f'Change by places first and last letters in each second string of list or array:\n'
       f'{swap_first_last_letter(list_of_strings)}')
 print(f'Revert strings of list or array:\n{revert_list_of_strings(list_of_strings)}')
